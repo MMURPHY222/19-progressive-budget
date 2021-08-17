14a57249c3cb1b3c2233398813ddea6cd8dfa0ac
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-16 21:34:28 -0400

    updated indexdb

--- public/indexdb.js
+++ public/indexdb.js
@@ -1,2 +1,71 @@
 let db;
 let budgetVer;
+
+const request = indexedDB.open('BudgetDB', budgetVer || 21);
+
+request.onupgradeneeded = function (event) {
+    console.log('Upgrade needed in IndexDB');
+  
+    const { oldVersion } = event;
+    const newVersion = event.newVersion || db.version;
+  
+    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+  
+    db = event.target.result;
+  
+    if (db.objectStoreNames.length === 0) {
+      db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
+};
+
+request.onerror = function (event) {
+    console.log(`${event.target.errorCode}`);
+};
+
+function checkDatabase() {
+    console.log('check db invoked');
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const store = transaction.objectStore('BudgetStore');
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((res) => {
+            if (res.length !== 0) {
+              transaction = db.transaction(['BudgetStore'], 'readwrite');
+              const currentStore = transaction.objectStore('BudgetStore');
+              currentStore.clear();
+              console.log('Clear store');
+            }
+          });
+      }
+    };
+};
+
+request.onsuccess = function (event) {
+    console.log('success');
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+      console.log('Backend online! 🗄️');
+      checkDatabase();
+    }
+  };
+
+  const saveRecord = (record) => {
+    console.log('Save record invoked');
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const store = transaction.objectStore('BudgetStore');
+    store.add(record);
+  };
+  
+  window.addEventListener('online', checkDatabase);
