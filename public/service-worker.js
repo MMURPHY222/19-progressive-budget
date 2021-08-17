d24cd6ca7844b4a2ef986224dda8ba6f08c95f22
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-16 21:42:39 -0400

    updated service worker w files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,9 @@
-const FILES_TO_CACHE = [];
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html", 
+    "/index.js",
+    "/styles.css"
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
