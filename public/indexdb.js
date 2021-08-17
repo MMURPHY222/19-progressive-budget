cac7f57b810f85959480488d8689d542995d48cb
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-17 17:16:28 -0400

    messed something up who knows where, redid some things

--- public/indexdb.js
+++ public/indexdb.js
@@ -1,71 +1,73 @@
 let db;
-let budgetVer;
+let budgetVersion;
 
-const request = indexedDB.open('BudgetDB', budgetVer || 21);
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
 
-request.onupgradeneeded = function (event) {
-    console.log('Upgrade needed in IndexDB');
-  
-    const { oldVersion } = event;
-    const newVersion = event.newVersion || db.version;
-  
-    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
-  
-    db = event.target.result;
-  
-    if (db.objectStoreNames.length === 0) {
-      db.createObjectStore('BudgetStore', { autoIncrement: true });
-    }
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB');
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
 };
 
-request.onerror = function (event) {
-    console.log(`${event.target.errorCode}`);
+request.onerror = function (e) {
+  console.log(`Woops! ${e.target.errorCode}`);
 };
 
 function checkDatabase() {
-    console.log('check db invoked');
-    let transaction = db.transaction(['BudgetStore'], 'readwrite');
-    const store = transaction.objectStore('BudgetStore');
-    const getAll = store.getAll();
-  
-    getAll.onsuccess = function () {
-      if (getAll.result.length > 0) {
-        fetch('/api/transaction/bulk', {
-          method: 'POST',
-          body: JSON.stringify(getAll.result),
-          headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((response) => response.json())
-          .then((res) => {
-            if (res.length !== 0) {
-              transaction = db.transaction(['BudgetStore'], 'readwrite');
-              const currentStore = transaction.objectStore('BudgetStore');
-              currentStore.clear();
-              console.log('Clear store');
-            }
-          });
-      }
-    };
-};
+  console.log('check db invoked');
 
-request.onsuccess = function (event) {
-    console.log('success');
-    db = event.target.result;
-  
-    if (navigator.onLine) {
-      console.log('Backend online! 🗄️');
-      checkDatabase();
+  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  const store = transaction.objectStore('BudgetStore');
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(['BudgetStore'], 'readwrite');
+            const currentStore = transaction.objectStore('BudgetStore');
+            currentStore.clear();
+            console.log('Clearing store 🧹');
+          }
+        });
     }
   };
+}
 
-  const saveRecord = (record) => {
-    console.log('Save record invoked');
-    const transaction = db.transaction(['BudgetStore'], 'readwrite');
-    const store = transaction.objectStore('BudgetStore');
-    store.add(record);
-  };
-  
-  window.addEventListener('online', checkDatabase);
+request.onsuccess = function (e) {
+  console.log('success');
+  db = e.target.result;
+  if (navigator.onLine) {
+    console.log('Backend online! 🗄️');
+    checkDatabase();
+  }
+};
+
+const saveRecord = (record) => {
+  console.log('Save record invoked');
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  const store = transaction.objectStore('BudgetStore');
+  store.add(record);
+};
+
+// Listen for app coming back online
+window.addEventListener('online', checkDatabase);
